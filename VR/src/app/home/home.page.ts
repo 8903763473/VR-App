bbf8766103f84fa93241085c289718cd58a1e551
Vijay <vijayakumar050301@gmail.com>
2024-01-18 12:12:43 +0530

    routing added for chat in home

--- VR/src/app/home/home.page.ts
+++ VR/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,8 +9,11 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {
+  constructor(public router: Router) {
 
   }
 
+  chat(data) {
+    this.router.navigate(['/' + data])
+  }
 }
